15f9cca04d89c714bbb55f27c4bef9ae1e683621
demafogotto <luppuno@gmail.com>
2024-02-27 18:41:06 +0300

    Step 5

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,6 @@ import readlineSync from 'readline-sync';
 const hello = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  const greeting = `Hello, ${name}!`;
-  console.log(greeting);
+  console.log(`Hello, ${name}!`);
 };
 export default hello;
